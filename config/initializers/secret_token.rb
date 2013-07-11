034712330e4e0b1abd1ca02735d6ecfaad573155
rexos <alex.pellegrini@live.com>
2013-07-11 14:32:42 +0200

    initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archimede::Application.config.secret_token = '9ca830168437281c62cb8d87a4f5d9b55f54b9ff8cf8d4379eb689925b766cb488add59ad75392b47208fd590f622361db1c8d377878d9be6975c405664c538c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Archimede::Application.config.session_store :cookie_store, key: '_archimede_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Archimede::Application.config.session_store :active_record_store
