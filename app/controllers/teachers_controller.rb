32ea58eb72cb38215f6487bb2e24311c576d6802
rexos <alex.pellegrini@live.com>
2013-07-17 19:02:10 +0200

    error management

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,9 +6,9 @@ class TeachersController < ApplicationController
   def create
     @teacher = Teacher.new( params[:teacher] )
     if @teacher.save
-      render action: :signup, :notice => "User correctly created"
+      render action: :signup, flash[:notice] => "User correctly created"
     else
-      render action: :signup, :notice => "User correctly created"
+      render action: :signup, flash[:notice] => "User not correctly created"
     end
   end
 
