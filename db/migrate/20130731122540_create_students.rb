2623cb3d6b650442a40c132e5e68c54a8caaa0dd
rexos <alex.pellegrini@live.com>
2013-07-31 14:39:44 +0200

    added student model

---
+++ db/migrate/20130731122540_create_students.rb
@@ -0,0 +1,13 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
