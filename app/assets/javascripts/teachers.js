07547dc4ad2702e35f90ec4dd283d0c7c50a582c
rexos <alex.pellegrini@live.com>
2013-07-31 22:21:48 +0200

    added replicate data js

---
+++ app/assets/javascripts/teachers.js
@@ -0,0 +1,10 @@
+$('#replicate_data').click( function(){
+	$('#teacher_bill_name').val( $('#teacher_name').val() );
+	$('#teacher_bill_last_name').val( $('#teacher_last_name').val() );
+	$('#teacher_bill_street').val( $('#teacher_street').val() );
+	$('#teacher_bill_country').val( $('#teacher_country').val() );
+	$('#teacher_bill_number').val( $('#teacher_number').val() );
+	$('#teacher_bill_city').val( $('#teacher_city').val() );
+	$('#teacher_bill_province').val( $('#teacher_province').val() );
+	$('#teacher_bill_cap').val( $('#teacher_cap').val() );
+} );
