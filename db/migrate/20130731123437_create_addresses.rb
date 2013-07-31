890442c3e9ace303a7796cab2081c3f67d8ce3e4
Francesco Serra <afnecors@gmail.com>
2013-07-31 19:25:53 +0200

    fattura drawer

--- db/migrate/20130731123437_create_addresses.rb
+++ db/migrate/20130731123437_create_addresses.rb
@@ -1,14 +1,14 @@
-class CreateAddresses < ActiveRecord::Migration
+ class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
       t.string :street
       t.integer :number
       t.integer :cap
       t.string :city
-      t.string :province
-      t.string :country
-      t.integer :student_id
-      t.integer :teacher_id
+      t.string :province,
+      t.string :country,
+      t.integer :student_id,
+      t.integer :teacher_id,
 
       t.timestamps
     end
