29d4dcacc1cc0e1a5fb112ee6cbbd8b496f7c5a4
Francesco Serra <afnecors@gmail.com>
2013-08-06 16:54:04 +0200

    add latitude and longitude to Address and fix minor

--- db/migrate/20130731123437_create_addresses.rb
+++ db/migrate/20130731123437_create_addresses.rb
@@ -7,6 +7,8 @@
       t.string :city
       t.string :province
       t.string :country
+      t.float :latitude
+      t.float :longitude
       t.integer :student_id
       t.integer :teacher_id
       t.integer :bill_id
