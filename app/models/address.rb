610d3d73011277c41748a35af806efe98c9e7a3a
rexos <alex.pellegrini@live.com>
2013-08-08 16:58:34 +0200

    added presence validations to bill and address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,29 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id         :integer          not null, primary key
+#  street     :string(255)
+#  number     :integer
+#  cap        :integer
+#  city       :string(255)
+#  province   :string(255)
+#  country    :string(255)
+#  latitude   :float
+#  longitude  :float
+#  student_id :integer
+#  teacher_id :integer
+#  bill_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Address < ActiveRecord::Base
   belongs_to :student, :class_name => 'Student', :foreign_key => 'student_id'
   belongs_to :teacher, :class_name => 'Teacher', :foreign_key => 'teacher_id'
   belongs_to :bill, :class_name => 'Bill', :foreign_key => 'bill_id'
   attr_accessible :cap, :city, :country, :number, :province, :street, :latitude, :longitude, :student_id, :teacher_id, :bill_id
+
+  validates_presence_of :cap, :city, :country, :province, :street
+
 end
